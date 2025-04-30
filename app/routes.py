a0e187a2765b2d2749981bd9b6f741736c64fe7e
Fabricio Braga <fabribraga@gmail.com>
2025-04-30 19:28:09 +0100

    final version

--- app/routes.py
+++ app/routes.py
@@ -7,8 +7,7 @@ from ibm_watson.natural_language_understanding_v1 import Features, CategoriesOpt
 # Set up Watson NLU
 # authenticator = IAMAuthenticator('your_api_key')  # Replace with your API key
 
-MY_API_KEY = 'UhqDCUyxsCOeJs_5aOVBdM0fdby96dOseatMernqVcxA'
-# UhqDCUyxsCOeJs_5aOVBdM0fdby96dOseatMernqVcxA
+MY_API_KEY = 'your key here'
 SERVICE_URL = 'https://eu-de.ml.cloud.ibm.com'
 
 authenticator = IAMAuthenticator(MY_API_KEY)  # Replace with your API key
